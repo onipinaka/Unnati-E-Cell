f30efb2e9815255eea9bbc5921141f81634790a5
Varchasv Sharma <varch.1906@gmail.com>
2025-08-02 23:11:37 +0530

    Added a basic form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import Form from "./components/Form"
+import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import Part3 from "./components/Part3"
+import Part4 from "./components/Part4"
 
 
 const App = () => {
   return (
    
-      <div className='flex flex-col w-[90vw]'>
+      <div className='flex flex-col gap-y-16 w-[90vw]'>
 
       <Navbar />
       <Part3/>
-      <Form />
+      <Part4/>
+      <Footer/>
       </div>
  
   )
