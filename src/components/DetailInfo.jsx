885bfb50d39984cb087c121f5d2cf12251d0651f
pinak <viveksharma_240087@aitpune.edu.in>
2025-08-05 19:02:24 +0530

    fixed minor issues

--- src/components/DetailInfo.jsx
+++ src/components/DetailInfo.jsx
@@ -6,8 +6,8 @@ function DetailInfo({title, description, reverse}) {
     <>
         <div style={reverse? {flexDirection: "row-reverse"}: {}} className='flex flex-row w-full justify-around '>
             <div className='lg:max-w-[30vw]'>
-              <p className='text-start font-semibold font-primary mb-2 text-[clamp(1.5rem,2vw,2.125rem)]'>{title}</p>
-              <p className='text-start font-normal font-primary text-[clamp(0.8rem,1.1vw,1.5rem)]'>{description}</p>
+              <p className='text-start font-semibold font-primary mb-2 text-black text-[clamp(1.5rem,2vw,2.125rem)]'>{title}</p>
+              <p className='text-start font-normal font-primary text-black text-[clamp(0.8rem,1.1vw,1.5rem)]'>{description}</p>
             </div>
             <div>
               <img src={img} alt="" />
